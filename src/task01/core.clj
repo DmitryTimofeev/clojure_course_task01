2edd6bd51dc33619d087c5865ca6151fba87f4b9
unknown <D_Timofeev@860f78455.KMS.GSS.local>
2013-03-22 19:00:59 +1100

    so close...

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -2,47 +2,23 @@
   (:require [pl.danieljanus.tagsoup :refer :all])
   (:gen-class))
 
-(defn foo [param1 param2]
-   (loop [head param1
-          body param2
+(defn foo [param]
+   (loop [head (first param)
+          body (next param)
           res []]
+
      (if (empty? body)
        res
-       (if (= (get head 1) {:class "r"})
-         (conj res (:href (get (get head 2) 1))))
-         (recur (first body) (next body) res))))
+       (do
+         (if (= (get head 1) {:class "r"})
+           (conj res (:href (get (get head 2) 1))))
+         (recur (first body) (next body) res)))))
 
 (defn get-links []
-
   (let [data (parse "clojure_google.html")]
-    (foo (first data) (next data))))
+    (foo data)))
+
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
 
-   (loop [head [1]
-          body [2 3 4 2 5 6]
-          res []]
-     (if (empty? body)
-       res
-       (if (=  head [2])
-         conj res (:href (get (get head 2) 1)))
-         (recur (first body) (next body) res)))
-"
-
-
-   (loop [head param1
-           body param2
-           res []]
-     (if (empty? (next body))
-       res
-       (if (= (get head 1) {:class "r"})
-         (conj res (:href (get (get head 2) 1))))
-         (recur (first body) (next body) res))))
-
-   (loop [head [1]
-           body [2 3 4 5 6]
-           res []]
-     (if (empty? (next body))
-       res
-       (recur (first body) (next body) res)))"
